e87ab73280338a166da9f39dfcb0da221eb53001
Twiyubake Innovation <brother.dushime@gmail.com>
2024-10-10 20:12:47 +0200

    new update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="w-[80%] bg-blue-400 h-[600px] rounded-[10px] flex items-center justify-center">
-      <h1 className="text-blue-700 font-bold tetx-[2.2rem]">Welcome to my document editting application</h1>
+    <div className="absolute w-[80%] bg-blue-400 h-[600px] rounded-[300px] flex items-center justify-center top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] ">
+      <h1 className="text-blue-700 font-extrabold text-[4rem] text-center">Welcome to my document editting application</h1>
     </div>
   );
 }
